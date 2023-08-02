02cd91e838abc1ab755f22d493a3413a01ff994b
santiagovincent13 <114445215+santiagovincent13@users.noreply.github.com>
2023-08-01 22:21:46 -0300

    Add files via upload
    vamo

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import "bootstrap-css-only";
+import "./App.css";
+import Layout from "../Pages/Layout";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Layout />
+
+      otra cosa
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Pages/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+
+const Layout = () => {
+    return (
+        <div>
+
+            <div>
+                Otra cosa Aca
+            </div>
+        </div>
+    )
+}
+export default Layout
